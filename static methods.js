6f87bf0cc4d284291a3db54ef5e9077f4af66789
Mohammed Harish <mohamharish0007@gmail.com>
2026-06-29 23:21:48 +0530

    Added basic programs

---
+++ static methods.js
@@ -0,0 +1,7 @@
+// Static Methods
+class MathHelper {
+  static add(a, b) { return a + b; }
+  static square(n) { return n * n; }
+}
+console.log("Static add:", MathHelper.add(3, 4));
+console.log("Static square:", MathHelper.square(5));
