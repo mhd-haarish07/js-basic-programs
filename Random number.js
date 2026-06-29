93751446482f46b8e51f6422322025d58cf77c43
Mohammed Harish <mohamharish0007@gmail.com>
2026-06-29 23:12:23 +0530

    Added basic programs

---
+++ Random number.js
@@ -0,0 +1,4 @@
+// Random Numbers
+console.log("random (0-1):", Math.random());
+console.log("random int 1-10:", Math.floor(Math.random() * 10) + 1);
+console.log("random int 1-100:", Math.floor(Math.random() * 100) + 1);
