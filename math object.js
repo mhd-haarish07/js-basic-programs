ab9a83151e3a6e33fd22ec0b9867a938cd819e18
Mohammed Harish <mohamharish0007@gmail.com>
2026-06-29 23:11:51 +0530

    Added basic programs

---
+++ math object.js
@@ -0,0 +1,16 @@
+// Math Object
+console.log("PI:", Math.PI);
+console.log("E:", Math.E);
+console.log("max:", Math.max(10, 20, 30));
+console.log("min:", Math.min(10, 20, 30));
+console.log("abs:", Math.abs(-45));
+console.log("round:", Math.round(4.5));
+console.log("floor:", Math.floor(4.9));
+console.log("ceil:", Math.ceil(4.1));
+console.log("trunc:", Math.trunc(4.9));   // 4
+console.log("sqrt:", Math.sqrt(144));
+console.log("cbrt:", Math.cbrt(27));      // 3
+console.log("pow:", Math.pow(2, 8));
+console.log("log:", Math.log(Math.E));    // 1
+console.log("log2:", Math.log2(8));       // 3
+console.log("log10:", Math.log10(1000));  // 3
