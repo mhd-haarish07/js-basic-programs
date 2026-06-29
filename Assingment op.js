3b38a590dc1189ac2ab8890ac17375bb42dab5e0
Mohammed Harish <mohamharish0007@gmail.com>
2026-06-29 23:11:16 +0530

    Added basic programs

---
+++ Assingment op.js
@@ -0,0 +1,7 @@
+// Assignment Operators
+let x = 10;
+x += 5;  console.log("+=:", x);
+x -= 3;  console.log("-=:", x);
+x *= 2;  console.log("*=:", x);
+x /= 4;  console.log("/=:", x);
+x **= 2; console.log("**=:", x);
